62fde81dad005ff262d081eb62ac7457ca8ee586
Michael Shamshidov <mshamshidov22@ku.edu.tr>
2024-02-14 14:26:49 +0500

    update

--- auth_service/util/auth_util.go
+++ auth_service/util/auth_util.go
@@ -14,14 +14,12 @@ var (
 	jwtSecretKey         = os.Getenv("AUTH_SERVICE_JWT_KEY")
 )
 
-// Claims represents the claims of the JWT token.
 type Claims struct {
 	UserID int64  `json:"user_id"`
 	Role   string `json:"role"`
 	jwt.StandardClaims
 }
 
-// GeneratePasswordHash generates a hash from a plaintext password.
 func GeneratePasswordHash(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -30,12 +28,10 @@ func GeneratePasswordHash(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
-// ComparePasswordAndHash compares a plaintext password with its hash.
 func ComparePasswordAndHash(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
 
-// GenerateTokens generates access and refresh tokens for a given user ID and role.
 func GenerateTokens(userID int64, role string) (string, string, error) {
 	accessToken, err := generateToken(userID, role, accessTokenDuration)
 	if err != nil {
@@ -50,7 +46,6 @@ func GenerateTokens(userID int64, role string) (string, string, error) {
 	return accessToken, refreshToken, nil
 }
 
-// generateToken generates a JWT token with the provided claims and expiration duration.
 func generateToken(userID int64, role string, duration time.Duration) (string, error) {
 	claims := Claims{
 		UserID: userID,
@@ -70,7 +65,6 @@ func generateToken(userID int64, role string, duration time.Duration) (string, e
 	return signedToken, nil
 }
 
-// ParseToken parses and validates a JWT token string.
 func ParseToken(tokenString string) (*Claims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(jwtSecretKey), nil
@@ -86,7 +80,6 @@ func ParseToken(tokenString string) (*Claims, error) {
 	return nil, jwt.ErrSignatureInvalid
 }
 
-// Middleware to extract JWT token from context
 func ExtractTokenFromContext(ctx context.Context) (string, error) {
 	token, ok := ctx.Value("token").(string)
 	if !ok {
